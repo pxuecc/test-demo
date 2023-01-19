0e483cdac5cbf086c148fee18474e2c4eb857833
pxuec <pxuec@outlook.com>
2023-01-19 12:04:00 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <canvas> 你的浏览器不支持 canvas，请升级你的浏览器。 </canvas>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+canvas {
+  background-color: pink;
+}
+</style>
